4c55064ca9b6a1913ea200e9f47bf214f96a9943
rachael-amuge <rachaelamuge12@gmail.com>
2021-03-11 16:21:52 +0300

    i have added buttons to the script

--- script.js
+++ script.js
@@ -1,37 +1,79 @@
-class calculator {
+class  Calculator{
     constructor(previousOperandTextElement,currentOperandTextElement){
         this.previousOperandTextElement = previousOperandTextElement
         this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
     }
     
     clear(){
-        this.previousOperand = ''
         this.currentOperand = ''
+        this.previousOperand = ''
         this.operation = 'undefined'
 
     }
 
     appendNumber(number){
-        this.currentOperand = this.currentOperand
+        if(number === '.' && this.currentOperand.inculdes('.')) return
+        this.currentOperand = this.currentOperand.tostring() + number.tostring()
     }
 
     delete(){
-
+        this.currentOperand = this.currentOperand.tostring().slice(0,-1)
     }
 
     chooseOperation(operation){
+        if (this.currentOperand === '') return
+        if(this.previousOperand !== ''){
+            this.compute()
+        }
+        this.operation = operation
+        this.previousOperand = this.previousOperand
+        this.currentOperand = ''
 
     }
 
     compute(){
+        let computation
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if(isNaN(prev) || isNaN(current)) return
+        switch(this.operation){
+            case '+':
+                computation = prev + current
+                break
+
+            case '-':
+                computation = prev - current
+                break
+                    
+            case '*':
+                computation = prev * current
+                break
+            case '÷':
+                computation = prev / current
+                break
+            default:
+                return
+        }
+
+        this.currentOperand = computation
+        this.operation = 'undefined'
+        this.previousOperand = ''
 
     }
 
     updateDisplay(){
-        this.previousOperand 
+        this.currentOperandTextElement.innerText = this.currentOperand
+        if(this.operation != null){
+            this.previousOperandTextElement.innerText =
+            `${this.previousOperand} ${this.operation}`
+        }
+        this.previousOperandTextElement.innerText = this.previousOperand
+
 
     }
 }
+
 
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
@@ -40,3 +82,35 @@ const deleteButton = document.querySelector('[data-delete]')
 const clearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+
+const calculator = new Calculator(previousOperandTextElement,currentOperandTextElement)
+
+numberButtons.forEach(button =>{
+    button.addEventListener('Click', () =>{
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
+    })
+})
+
+chooseOperation.forEach(button =>{
+    button.addEventListener('Click', () =>{
+        calculator.chooseOperation(button.innerText);
+        calculator.updateDisplay();
+    })
+})
+
+equalsButton.addEventListener('click',button =>{
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click',button =>{
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click',button =>{
+    calculator.delete()
+    calculator.updateDisplay()
+})
